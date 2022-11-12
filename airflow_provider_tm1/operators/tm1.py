10b83c6de4c93abe7f4d2fc9d4f42fe8d71f5974
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2022-11-13 00:03:37 +0100

    feat: remove decorator check pulse draft

--- airflow_provider_tm1/operators/tm1.py
+++ airflow_provider_tm1/operators/tm1.py
@@ -1,9 +1,38 @@
 from typing import Optional
 
 from airflow.models import BaseOperator
-from airflow.utils.decorators import apply_defaults
 
 from airflow_provider_tm1.hooks.tm1 import TM1Hook
+
+
+class TM1CheckPulseOperator(BaseOperator):
+    def __init__(
+        self,
+        process_name: str,
+        tm1_conn_id: str = "tm1_default",
+        parameters: Optional[dict] = None,
+        *args,
+        **kwargs,
+    ) -> None:
+
+        super().__init__(*args, **kwargs)
+
+        self.tm1_conn_id = tm1_conn_id
+        self.process_name = process_name
+        self.parameters = parameters
+
+    def execute(self, context: dict) -> None:
+        tm1_hook = TM1Hook(tm1_conn_id=self.tm1_conn_id)
+
+        tm1 = tm1_hook.get_conn()
+
+        if not tm1.processes.exists(self.process_name):
+            raise Exception(f"Process {self.process_name} not found on TM1 server {tm1_hook.db}.")
+        else:
+            print(
+                f"Process {self.process_name} executed on TM1 server {tm1_hook.db} with parameters {self.parameters}."
+            )
+            tm1.processes.execute(self.process_name, **self.parameters)
 
 
 class TM1RunTIOperator(BaseOperator):
@@ -16,7 +45,6 @@ class TM1RunTIOperator(BaseOperator):
     :type tm1_conn_id: str
     """
 
-    @apply_defaults
     def __init__(
         self,
         process_name: str,
@@ -56,7 +84,6 @@ class TM1RunChoreOperator(BaseOperator):
     :type tm1_conn_id: str
     """
 
-    @apply_defaults
     def __init__(self, chore_name: str, tm1_conn_id: str = "tm1_default", *args, **kwargs) -> None:
 
         super().__init__(*args, **kwargs)
