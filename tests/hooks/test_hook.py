7b85de9b4f3a66315a3a65c200226cd0b932468f
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2022-11-12 14:18:21 +0100

    fix: only run mock tests in action

--- tests/hooks/test_hook.py
+++ tests/hooks/test_hook.py
@@ -1,10 +1,12 @@
 from unittest import mock
 
+import pytest
 from airflow.models.connection import Connection
 
 from airflow_provider_tm1.hooks.tm1 import TM1Hook
 
 
+@pytest.mark.mocked
 def test_mock_connection(mock_connection_uri):
 
     with mock.patch.dict("os.environ", AIRFLOW_CONN_TM1_DEFAULT=mock_connection_uri):
