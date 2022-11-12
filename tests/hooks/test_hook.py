67222a239f1d8da452b9dd2e0e59857cc2c5d177
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2022-11-12 14:11:36 +0100

    feat: hook proof of concept

--- tests/hooks/test_hook.py
+++ tests/hooks/test_hook.py
@@ -1,8 +1,22 @@
 from airflow_provider_tm1.hooks.tm1 import TM1Hook
 
+from unittest import mock
 
-def test_hook_init():
+from airflow.models.connection import Connection
 
-    hook = TM1Hook()
+def test_mock_connection(mock_connection_uri):
 
-    assert hook
+    with mock.patch.dict("os.environ", AIRFLOW_CONN_TM1_DEFAULT=mock_connection_uri):
+        
+        Connection.get_connection_from_secrets(conn_id="tm1_default")
+
+        assert Connection.get_connection_from_secrets(conn_id="tm1_default").conn_type == "tm1"
+
+
+def test_hook_init(mock_connection_uri):
+
+    with mock.patch.dict("os.environ", AIRFLOW_CONN_TM1_DEFAULT=mock_connection_uri):
+        
+        Connection.get_connection_from_secrets(conn_id="tm1_default")
+
+        assert TM1Hook(tm1_conn_id="tm1_default")
